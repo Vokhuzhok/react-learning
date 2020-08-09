976988b1e43c2e2430bb215357322282205900a7
Denis <kurkoja@bk.ru>
2020-08-09 11:53:34 +0300

    Added new files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src='http://www.usb-over-network.com/img/main/fabulatech-logo-1600.png'></img>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+            <div className='profileimg'>
+                <img src='https://im0-tub-ru.yandex.net/i?id=eaf56b9e7beb2e7d7b8d1f498d384ae6&n=13&exp=1'></img>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
