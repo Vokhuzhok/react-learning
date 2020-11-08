2961710a502ffb60f720de1cfc73571f993c8187
Denis Kozhukhov <kurkoja@bk.ru>
2020-11-08 19:07:41 +0300

    Bug fix...

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,54 +1,61 @@
-import React from 'react';
-import s from './Users.module.css'
+import React from "react";
+import s from "./Users.module.css";
 import * as axios from "axios";
-
+import noUserPhoto from "../../assest/images/User.png";
 
 const Users = (props) => {
 
-let getUsers = () => {
-    if (props.users.lenght === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-    .then (response => {
-        props.setUsers(response.data.item)
-    });
-}
-};
-
-return (
+  let getUsers = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+        
+          props.setUsers(response.data.items);
+        });
+    }
+  };
+  return (
     <div>
-        <button onClick = {getUsers}>Get Users</button>
-        {
-            props.users.map (ue => <div key = {ue.id} className = {s.users}> 
-            <div className = {s.left}> 
-                <div className = {s.ava}>
-                    <img src={ue.photos.small}/>
-                </div>
-                <div>
-                    {ue.name}
-                </div>
-                <div className = {s.button}>
-                   {ue.followed ? 
-                   <button onClick = { () => { props.unfollow (ue.id) } }>Unfollow</button> :
-                    <button onClick = { () => { props.follow (ue.id) } }>Follow</button>}
-                </div>                
+      <button onClick={getUsers}>Get Users</button>
+      {props.users.map((ue) => (
+        <div key={ue.id} className={s.users}>
+          <div className={s.left}>
+            <div className={s.ava}>
+              <img src={ue.photos.small != null ? ue.photos.small: noUserPhoto} />
             </div>
-            <div className = {s.center}>
-                <div className = {s.status}>
-                    {ue.status}
-                </div>
+            <div>{ue.name}</div>
+            <div className={s.button}>
+              {ue.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollow(ue.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.follow(ue.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
             </div>
-            <div className = {s.right}>
-                <div>
-                    {"city"}
-                </div>
-                <div>
-                    {"country"}
-                </div>
-            </div>
-            </div>)
-        }
+          </div>
+          <div className={s.center}>
+            <div className={s.status}>{ue.status}</div>
+          </div>
+          <div className={s.right}>
+            <div>{"city"}</div>
+            <div>{"country"}</div>
+          </div>
+        </div>
+      ))}
     </div>
-)
-}
+  );
+};
 
 export default Users;
