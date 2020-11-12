5683af894b2f16abb5133bd26c81396264fabc8a
Denis Kozhukhov <kurkoja@bk.ru>
2020-11-12 18:10:53 +0300

    Min

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,8 +4,7 @@ import * as axios from "axios";
 import noUserPhoto from "../../assest/images/User.png";
 
 class Users extends React.Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
     axios
       .get("https://social-network.samuraijs.com/api/1.0/users")
       .then((response) => {
